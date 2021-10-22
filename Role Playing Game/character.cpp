8902d396cc4c316e0b2a3cd6c09948abc988c6ed
Oryto Mikli <orytomikli21@gmail.com>
2021-10-22 16:26:39 +0700

    Add Soal and Give character.cpp something to work

--- Role Playing Game/character.cpp
+++ Role Playing Game/character.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
+void PrintName(string pname){
+  cout << "Hello! " << pname << " Wellcome to CRPG" << endl;
+}
+
+unsigned long long Fibbonaci(int numbern){
+  unsigned long long n2n1[2] = {0,1};
+  unsigned long long  n = 0;
+  for(int i = 0; i < numbern; i++){
+    n = n2n1[0] + n2n1[1];
+    n2n1[0] = n2n1[1];
+    n2n1[1] = n;
+  }
+  return n;
+}
+
 int main()
 {
   cout << "Wellcome To RolerBlade!" <<endl;
-  string name = "Nill";
+  string name; 
+  cout << "Type your player name: ";
+  getline(cin, name);
+  PrintName(name); 
+
+  cout << Fibbonaci(100) << endl;
+
+  return 0;
 }
