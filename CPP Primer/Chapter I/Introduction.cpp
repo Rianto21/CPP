f28e64bfbfe1142fa5aa34f92c8b76b35eb55350
Oryto Mikli <orytomikli21@gmail.com>
2021-11-15 01:02:27 +0700

    mantab

---
+++ CPP Primer/Chapter I/Introduction.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+  
+  // Flow Control
+  // if you need to collect all input
+
+  int sum = 0, value = 0;
+  while (cin>>value) // it never stop until you hit end of file if it txt or make an error
+  {
+    sum += value;
+  }
+  cout << "Total Input is: " << sum << endl;
+
+
+  //
+
+
+
+
+  return 0;
+}
